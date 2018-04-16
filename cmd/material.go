ef67ac52cdf5eda0645847f2d80fbff2bef9c589
mizzky <ein.dreieck1212@icloud.com>
2018-04-17 00:03:43 +0900

    fix typo

--- cmd/material.go
+++ cmd/material.go
@@ -4,7 +4,7 @@ package cmd
 
 import "fmt"
 
-var (
+const (
 
 	str_01 = 
 " 	　　. ＼：／ \n
---
+++ material_main.go
@@ -0,0 +1,11 @@
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("%s", cmd.str_01)
+}

13e834b40d380b56d92159659727d7efdd7d397b
mizzky <ein.dreieck1212@icloud.com>
2018-08-12 16:44:21 +0900

    test from jetbrain

--- prompt/main.go
+++ prompt/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"path/filepath"
 )
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println(getdir("./"))
+}
+
+func getdir(dir string) []string {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	var paths []string
+	for _, file := range files {
+		if file.IsDir() {
+			paths = append(paths, getdir(filepath.Join(dir, file.Name()))...)
+			continue
+		}
+		paths = append(paths, filepath.Join(dir, file.Name()))
+	}
+
+	return paths
 }
